31b99a964e685ca315a4f5c02b613e2cc3629e90
Minchu <minchu@Minchus-MacBook-Pro.local>
2016-05-06 21:51:24 +0530

    Added pom.xml

--- src/main/java/smartcity/Database.java
+++ src/main/java/smartcity/Database.java
@@ -12,7 +12,6 @@ public class Database {
     public static DB db = mongo.getDB("smartcitydb");
 
     public static DBCollection allworks = db.getCollection("allworks");
-    public static DBCollection corporatorsCollection = db.getCollection("corporatorsC");
     public static DBCollection workDetailsCollection = db.getCollection("workdetails");
     public static DBCollection wardmaster = db.getCollection("wardmaster");
 
